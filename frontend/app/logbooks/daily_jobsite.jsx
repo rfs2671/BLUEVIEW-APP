4480dd5e049b03c5ebee3098c6b6ae8aced56fc4
rfs2671 <156490136+rfs2671@users.noreply.github.com>
2026-03-18 22:11:47 -0400

    Fix formatting issues in daily_jobsite.jsx

--- frontend/app/logbooks/daily_jobsite.jsx
+++ frontend/app/logbooks/daily_jobsite.jsx
@@ -643,6 +643,7 @@ export default function DailyJobsiteLog() {
               style={s.submitBtn}
             />
           </View>
+        </ScrollView>
         <FloatingNav />
       </SafeAreaView>
 
